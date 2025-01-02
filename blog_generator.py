e9bb77207bd90beb6f8ebac7b232c3a8c0910370
edwardlancelorilla2048 <edwardlorilla2048@gmail.com>
2025-01-02 21:52:26 +0800

    Update blog_generator.py

--- blog_generator.py
+++ blog_generator.py
@@ -994,7 +994,8 @@ if __name__ == "__main__":
     # List of recipients
     recipients = [
         "edwardlorilla2048.edwardlancelorilla@blogger.com",
-        "edwardlorilla2064.edwardlancelorilla@blogger.com"
+        "edwardlorilla2064.edwardlancelorilla@blogger.com",
+        "edwardlorilla2064.edwardlancelorilla1@blogger.com",
     ]
     for recipient in recipients:
         topic, word_count, audience = generate_random_inputs()
